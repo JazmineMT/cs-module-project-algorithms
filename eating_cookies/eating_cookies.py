dfff5034f2f61f35b7b6b7335312a0fcf4fb1dfb
JazmineMT <jazmineamcginnis@gmail.com>
2020-09-17 11:00:25 -0700

    all tests working , sliding window slow

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,34 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, memory=None):
     # Your code here
 
-    pass
+    # a case for zero cookies 
+   
+    # a case for a negative number
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    
+    elif n == 3:
+        return 4
+    
+    elif memory and memory[n] > 0:
+        return memory[n]
+    else:
+        if not memory:
+            memory = {i: 0 for i in range(n+1)}
+        memory[n] = eating_cookies(n-1, memory) + eating_cookies(n-2, memory) + eating_cookies(n-3, memory)
+    return memory[n]
+        
+    
+        
+
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
